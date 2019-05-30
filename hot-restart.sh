70a5ee05837baa5e1eee34656bfb563deb9a8440
Björn Wenzel <bjoern-wenzel@hotmail.de>
2019-05-30 23:17:05 +0200

    Fix changed variables with newer envoy version

--- hot-restart.sh
+++ hot-restart.sh
@@ -2,4 +2,4 @@
 
 ulimit -n 102400
 
-exec /usr/local/bin/envoy --v2-config-only -l $loglevel -c /etc/envoy/envoy.yaml --restart-epoch $RESTART_EPOCH --drain-time-s 300 --parent-shutdown-time-s 320
+exec /usr/local/bin/envoy --config-path /etc/envoy/envoy.yaml --restart-epoch $RESTART_EPOCH --drain-time-s 300 --parent-shutdown-time-s 320
